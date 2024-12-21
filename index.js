8ce8519ca547cf1dee2353b523a76b033c08b2ab
sam-shervin <samshervin.sebastian@gmail.com>
2024-12-21 07:26:53 +0530

    add Prisma integration with PostgreSQL and set up basic API endpoints

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const express = require('express')
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const prisma = new PrismaClient();
 const app = express();
 const port = 8032;
 
-app.get('/', (req, res) => {
-    res.send('Server is running on port 8032');
+app.use(express.json());
+
+//cors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.get("/test", (req, res) => {
+  try {
+    res.send("Hello World");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
